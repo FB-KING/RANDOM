62730412e7d9d15ec6d3fd34d8cee390fd0f1689
Mahin Ahmed <106468659+FB-KING@users.noreply.github.com>
2022-09-16 16:37:30 +0600

    Update FB-KING.py

--- FB-KING.py
+++ FB-KING.py
@@ -6,8 +6,8 @@ try:
  
 except:
  
-        os.system('pip2 install requests');time.sleep(2)
-        print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m');time.sleep(2)
+        os.system('pip2 install requests')
+
         os.system('xdg-open https://github.com/FB-KING')
  
  
